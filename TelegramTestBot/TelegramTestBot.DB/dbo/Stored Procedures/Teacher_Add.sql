310667a34dd0b9e3ee0af2163d8113edebec474c
Maxim Bespalov <i.maksim_01@mail.ru>
2023-02-23 16:52:56 +0300

    add stored procedures for Teacher

---
+++ TelegramTestBot/TelegramTestBot.DB/dbo/Stored Procedures/Teacher_Add.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[Teacher_Add]
+	@FirstName nvarchar(30),
+	@LastName nvarchar(30),
+	@SurName nvarchar(30),
+	@Email nvarchar(30),
+	@Login nvarchar(30),
+	@Password nvarchar(30)
+AS
+BEGIN
+INSERT INTO dbo.[Teacher](
+	FirstName,
+	LastName,
+	SurName,
+	Email,
+	[Login],
+	[Password])
+VALUES(
+	@FirstName,
+	@LastName,
+	@SurName,
+	@Email,
+	@Login,
+	@Password)
+SELECT @@IDENTITY
+END
---
+++ TelegramTestBot/TelegramTestBot.DB/dbo/Stored Procedures/Teacher_UpdateById.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[Teacher_UpdateById]
+	@Id int,
+	@FirstName nvarchar(30),
+	@LastName nvarchar(30),
+	@SurName nvarchar(30),
+	@Email nvarchar(30),
+	@Login nvarchar(30),
+	@Password nvarchar(30)
+
+AS
+BEGIN
+
+UPDATE dbo.[Teacher]
+SET FirstName = @FirstName,
+	LastName = @LastName,
+	SurName = @SurName,
+	Email = @Email,
+	[Login] = @Login,
+	[Password] = @Password 
+WHERE Id = @Id
+
+END
