5fe26f2ad65fa67952064b1a8d11372ed9583792
Maxim Bespalov <i.maksim_01@mail.ru>
2023-02-28 15:21:39 +0300

    create .DAL project

---
+++ TelegramTestBot/TelegramTestBot.DAL/StoredProcedures.cs
@@ -0,0 +1,11 @@
+﻿namespace TelegramTestBot.DAL
+{
+    public class StoredProcedures
+    {
+        public const string AnswerVariant_Add = "AnswerVariant_Add";
+        public const string AnswerVariant_DeleteById = "AnswerVariant_DeleteById";
+        public const string AnswerVariant_GetAll = "AnswerVariant_GetAll";
+        public const string AnswerVariant_GetById = "AnswerVariant_GetById";
+        public const string AnswerVariant_UpdateById = "AnswerVariant_UpdateById";
+    }
+}
