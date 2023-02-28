abda6119f54c4b7fface5583c93f288909bea385
Maxim Bespalov <i.maksim_01@mail.ru>
2023-02-28 15:27:22 +0300

    add const strings for sp

--- TelegramTestBot/TelegramTestBot.DAL/StoredProcedures.cs
+++ TelegramTestBot/TelegramTestBot.DAL/StoredProcedures.cs
@@ -7,5 +7,47 @@
         public const string AnswerVariant_GetAll = "AnswerVariant_GetAll";
         public const string AnswerVariant_GetById = "AnswerVariant_GetById";
         public const string AnswerVariant_UpdateById = "AnswerVariant_UpdateById";
+
+        public const string Group_Add = "Group_Add";
+        public const string Group_DeleteById = "Group_DeleteById";
+        public const string Group_GetAll = "Group_GetAll";
+        public const string Group_GetById = "Group_GetById";
+        public const string Group_UpdateById = "Group_UpdateById";
+
+        public const string Question_Add = "Question_Add";
+        public const string Question_DeleteById = "Question_DeleteById";
+        public const string Question_GetAll = "Question_GetAll";
+        public const string Question_GetById = "Question_GetById";
+        public const string Question_UpdateById = "Question_UpdateById";
+
+        public const string Student_Add = "Student_Add";
+        public const string Student_DeleteById = "Student_DeleteById";
+        public const string Student_GetAll = "Student_GetAll";
+        public const string Student_GetById = "Student_GetById";
+        public const string Student_UpdateById = "Student_UpdateById";
+
+        public const string Teacher_Add = "Teacher_Add";
+        public const string Teacher_DeleteById = "Teacher_DeleteById";
+        public const string Teacher_GetAll = "Teacher_GetAll";
+        public const string Teacher_GetById = "Teacher_GetById";
+        public const string Teacher_UpdateById = "Teacher_UpdateById";
+
+        public const string Test_Add = "Test_Add";
+        public const string Test_DeleteById = "Test_DeleteById";
+        public const string Test_GetAll = "Test_GetAll";
+        public const string Test_GetById = "Test_GetById";
+        public const string Test_UpdateById = "Test_UpdateById";
+
+        public const string Testing_Add = "Testing_Add";
+        public const string Testing_DeleteById = "Testing_DeleteById";
+        public const string Testing_GetAll = "Testing_GetAll";
+        public const string Testing_GetById = "Testing_GetById";
+        public const string Testing_UpdateById = "Testing_UpdateById";
+
+        public const string TypeOfQuestion_Add = "TypeOfQuestion_Add";
+        public const string TypeOfQuestion_DeleteById = "TypeOfQuestion_DeleteById";
+        public const string TypeOfQuestion_GetAll = "TypeOfQuestion_GetAll";
+        public const string TypeOfQuestion_GetById = "TypeOfQuestion_GetById";
+        public const string TypeOfQuestion_UpdateById = "TypeOfQuestion_UpdateById";
     }
 }
