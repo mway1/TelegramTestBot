5108286654743d85ef414e8bebb79b2cfc997296
Maxim Bespalov <i.maksim_01@mail.ru>
2023-03-03 15:41:49 +0300

    complete GroupManager

--- TelegramTestBot/TelegramTestBot.DAL/Managers/GroupManager.cs
+++ TelegramTestBot/TelegramTestBot.DAL/Managers/GroupManager.cs
@@ -5,7 +5,89 @@ using TelegramTestBot.DAL.Interfaces;
 
 namespace TelegramTestBot.DAL.Managers
 {
-    public class GroupManager
+    public class GroupManager : IGroupManager
     {
+        public void AddGroup(GroupDTO newGroup)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingle<GroupDTO>
+                    (
+                        StoredProcedures.Group_Add,
+                        param: new
+                        {
+                            Name = newGroup.Name,
+                            StudentId = newGroup.Student!.Id
+                        },
+                        commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void DeleteGroupById(int groupId)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingle<GroupDTO>
+                    (
+                        StoredProcedures.Group_DeleteById,
+                        param: new { id = groupId },
+                        commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void UpdateGroupById(GroupDTO newGroup)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingleOrDefault<GroupDTO>
+                    (
+                        StoredProcedures.Group_UpdateById,
+                        param: new
+                        {
+                            newGroup.Id,
+                            newGroup.Name,
+                            StudentId = newGroup.Student!.Id
+                        },
+                        commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public List<GroupDTO> GetAllGroups()
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                return connection.Query<GroupDTO>
+                    (
+                        StoredProcedures.Group_GetAll,
+                        commandType: System.Data.CommandType.StoredProcedure
+                    ).ToList();
+            }
+        }
+
+        public GroupDTO GetGroupById(int groupId)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                return connection.QuerySingle<GroupDTO>
+                    (
+                        StoredProcedures.Group_GetById,
+                        param: new { id = groupId },
+                        commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
     }
 }
