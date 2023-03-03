b8d7b1e756665f684e5d1da332048e67e7fc34f9
Maxim Bespalov <i.maksim_01@mail.ru>
2023-03-03 15:57:40 +0300

    complete student and teacher managers

--- TelegramTestBot/TelegramTestBot.DAL/Managers/TeacherManager.cs
+++ TelegramTestBot/TelegramTestBot.DAL/Managers/TeacherManager.cs
@@ -5,7 +5,97 @@ using TelegramTestBot.DAL.Interfaces;
 
 namespace TelegramTestBot.DAL.Managers
 {
-    public class TeacherManager
+    public class TeacherManager : ITeacherManager
     {
+        public void AddTeacher(TeacherDTO newTeacher)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingle<TeacherDTO>
+                    (
+                        StoredProcedures.Teacher_Add,
+                        param: new
+                        {
+                            FirstName = newTeacher.FirstName,
+                            LastName = newTeacher.LastName,
+                            SurName = newTeacher.SurName,
+                            Email = newTeacher.Email,
+                            Login = newTeacher.Login,
+                            Password = newTeacher.Password
+                        },
+                        commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void DeleteTeacherById(int teacherId)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingle<TeacherDTO>
+                    (
+                        StoredProcedures.Teacher_DeleteById,
+                        param: new { id = teacherId },
+                        commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public void UpdateTeacherById(TeacherDTO newTeacher)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                connection.QuerySingleOrDefault<TeacherDTO>
+                    (
+                        StoredProcedures.Teacher_UpdateById,
+                        param: new
+                        {
+                            newTeacher.Id,
+                            newTeacher.FirstName,
+                            newTeacher.LastName,
+                            newTeacher.SurName,
+                            newTeacher.Email,
+                            newTeacher.Login,
+                            newTeacher.Password
+                        },
+                        commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
+
+        public List<TeacherDTO> GetAllTeachers()
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                return connection.Query<TeacherDTO>
+                    (
+                        StoredProcedures.Teacher_GetAll,
+                        commandType: System.Data.CommandType.StoredProcedure
+                    ).ToList();
+            }
+        }
+
+        public TeacherDTO GetTeacherById(int teacherId)
+        {
+            using (var connection = new SqlConnection(ServerSettings._connectionString))
+            {
+                connection.Open();
+
+                return connection.QuerySingle<TeacherDTO>
+                    (
+                        StoredProcedures.Teacher_GetById,
+                        param: new { id = teacherId },
+                        commandType: System.Data.CommandType.StoredProcedure
+                    );
+            }
+        }
     }
 }
