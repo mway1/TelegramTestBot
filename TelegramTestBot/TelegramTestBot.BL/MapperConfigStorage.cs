42f11ea7192cb2dc16dd05d4fb990494bd193f0c
Maxim Bespalov <i.maksim_01@mail.ru>
2023-03-03 20:31:35 +0300

    complete MCS

--- TelegramTestBot/TelegramTestBot.BL/MapperConfigStorage.cs
+++ TelegramTestBot/TelegramTestBot.BL/MapperConfigStorage.cs
@@ -25,6 +25,58 @@ namespace TelegramTestBot.BL
                 .ForMember("CorrectAnswer", opt => opt.MapFrom(a => a.CorrectAnswer))
                 .ForMember("QuestionId", opt => opt.MapFrom(a => a.QuestionId))
                 .ReverseMap();
+
+                cfg.CreateMap<QuestionDTO, QuestionModel>()
+                .ForMember("Id", opt => opt.MapFrom(q => q.Id))
+                .ForMember("Content", opt => opt.MapFrom(q => q.Content))
+                .ForMember("TestId", opt => opt.MapFrom(q => q.TestId))
+                .ForMember("TypeOfQuestionId", opt => opt.MapFrom(q => q.TypeOfQuestionId))
+                .ReverseMap();
+
+                cfg.CreateMap<GroupDTO, GroupModel>()
+                .ForMember("Id", opt => opt.MapFrom(g => g.Id))
+                .ForMember("Name", opt => opt.MapFrom(g => g.Name))
+                .ForMember("StudentId", opt => opt.MapFrom(g => g.StudentId))
+                .ReverseMap();
+
+                cfg.CreateMap<StudentDTO, StudentModel>()
+                .ForMember("Id", opt => opt.MapFrom(s => s.Id))
+                .ForMember("FirstName", opt => opt.MapFrom(s => s.FirstName))
+                .ForMember("LastName", opt => opt.MapFrom(s => s.LastName))
+                .ForMember("SurName", opt => opt.MapFrom(s => s.SurName))
+                .ForMember("Username", opt => opt.MapFrom(s => s.Username))
+                .ForMember("Attendance", opt => opt.MapFrom(s => s.Attendance))
+                .ReverseMap();
+
+                cfg.CreateMap<TeacherDTO, TeacherModel>()
+                .ForMember("Id", opt => opt.MapFrom(t => t.Id))
+                .ForMember("FirstName", opt => opt.MapFrom(t => t.FirstName))
+                .ForMember("LastName", opt => opt.MapFrom(t => t.LastName))
+                .ForMember("SurName", opt => opt.MapFrom(t => t.SurName))
+                .ForMember("Email", opt => opt.MapFrom(t => t.Email))
+                .ForMember("Login", opt => opt.MapFrom(t => t.Login))
+                .ForMember("Password", opt => opt.MapFrom(t => t.Password))
+                .ReverseMap();
+
+                cfg.CreateMap<TestDTO, TestModel>()
+                .ForMember("Id", opt => opt.MapFrom(tt => tt.Id))
+                .ForMember("Name", opt => opt.MapFrom(tt => tt.Name))
+                .ForMember("TeacherId", opt => opt.MapFrom(tt => tt.TeacherId))
+                .ReverseMap();
+
+                cfg.CreateMap<TestingDTO, TestingModel>()
+                .ForMember("Id", opt => opt.MapFrom(tg => tg.Id))
+                .ForMember("Date", opt => opt.MapFrom(tg => tg.Date))
+                .ForMember("Start", opt => opt.MapFrom(tg => tg.Start))
+                .ForMember("End", opt => opt.MapFrom(tg => tg.End))
+                .ForMember("GroupId", opt => opt.MapFrom(tg => tg.GroupId))
+                .ForMember("TestId", opt => opt.MapFrom(tg => tg.TestId))
+                .ReverseMap();
+
+                cfg.CreateMap<TypeOfQuestionDTO, TypeOfQuestionModel>()
+                .ForMember("Id", opt => opt.MapFrom(toq => toq.Id))
+                .ForMember("Name", opt => opt.MapFrom(toq => toq.Name))
+                .ReverseMap();
             }));
         }
     }
