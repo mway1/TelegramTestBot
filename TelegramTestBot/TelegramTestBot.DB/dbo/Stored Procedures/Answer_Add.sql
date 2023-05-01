617279c8b8f289199f971435d6588d2b75d5483f
Maxim Bespalov <i.maksim_01@mail.ru>
2023-05-01 16:24:00 +0300

    edit sp for table Answer

---
+++ TelegramTestBot/TelegramTestBot.DB/dbo/Stored Procedures/Answer_Add.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[Answer_Add]
+	@Content nvarchar(50),
+	@IsCorrect bit,
+	@QuestionId int
+AS
+BEGIN
+INSERT INTO dbo.[Answer](
+	[Content],
+	IsCorrect,
+	QuestionId)
+VALUES(
+	@Content,
+	@IsCorrect,
+	@QuestionId)
+SELECT @@IDENTITY
+END
---
+++ TelegramTestBot/TelegramTestBot.DB/dbo/Stored Procedures/Answer_GetById.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[Answer_GetById]
+	@Id int
+AS
+BEGIN
+
+	SELECT Id, Content, IsCorrect, QuestionId
+	FROM dbo.[Answer]
+	WHERE Id=@Id
+
+END
