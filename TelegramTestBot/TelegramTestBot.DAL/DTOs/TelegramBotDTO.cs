8c1edfca066839d8bec1978690ca79a25791f8ee
Maxim Bespalov <i.maksim_01@mail.ru>
2023-05-02 13:55:36 +0300

    add table, sp and dal for hashToken telegram bot

---
+++ TelegramTestBot/TelegramTestBot.DAL/DTOs/TelegramBotDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace TelegramTestBot.DAL.DTOs
+{
+    public class TelegramBotDTO
+    {
+        public int Id { get; set; }
+        public string HashToken { get; set; }
+
+        public TelegramBotDTO() { }
+    }
+}
---
+++ TelegramTestBot/TelegramTestBot.DAL/Interfaces/ITelegramBotManager.cs
@@ -0,0 +1,11 @@
+﻿using TelegramTestBot.DAL.DTOs;
+
+namespace TelegramTestBot.DAL.Interfaces
+{
+    public interface ITelegramBotManager
+    {
+        void AddTelegramBot(TelegramBotDTO newTelegramBot);
+        void DeleteByIdTelegramBot(int telegramBotId);
+        TelegramBotDTO GetTelegramBotById(int telegramBotId);
+    }
+}
