b7fec09b49659993af3bf565804d33339ae5071a
Maxim Kuzmenko <mway2914@yandex.ru>
2023-05-04 17:14:53 +0300

    edit form of registration and start release registration process

--- TelegramTestBot/TelegramTestBot.UI/MainWindow.xaml.cs
+++ TelegramTestBot/TelegramTestBot.UI/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TelegramTestBot.BL.Models;
+using TelegramTestBot.BL.Managers;
 
 namespace TelegramTestBot.UI
 {
@@ -23,6 +25,10 @@ namespace TelegramTestBot.UI
         {
             InitializeComponent();
         }
+
+        private TeacherModelManager _teacherModelManager = new TeacherModelManager();  
+
+       
 
         private void B_signin_Click(object sender, RoutedEventArgs e)
         {
