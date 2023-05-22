b5ffd46efea159b9e2542a07fafe71f04ea1f97a
Maxim Kuzmenko <mway2914@yandex.ru>
2023-05-23 00:24:42 +0300

    clear code, edit exception and add creating test,edit and delete

--- TelegramTestBot/TelegramTestBot.UI/MainWindow.xaml.cs
+++ TelegramTestBot/TelegramTestBot.UI/MainWindow.xaml.cs
@@ -28,13 +28,13 @@ namespace TelegramTestBot.UI
         private int _createdTestId;
         private int _updatedQuestId;
         private int _updatedTestId;
+        private int _updatedGroupId;
         private List<string> _labels;
         private TeacherModelManager _teacherModelManager = new TeacherModelManager();
         private TestModelManager _testModelManager = new TestModelManager();
         private QuestionModelManager _questionModelManager = new QuestionModelManager();
         private AnswerModelManager _answerModelManager = new AnswerModelManager();
-        private List<QuestionModel> _allQuest = new List<QuestionModel>();
-        private List<AnswerModel> _allAnswer = new List<AnswerModel>();
+        private GroupModelManager _groupModelManager = new GroupModelManager();
         List<AnswerModel> _answersForEditQuest = new List<AnswerModel>();
         private TelegramBotService _telegramBotService;
         private TestService testService = new TestService();
@@ -212,58 +212,63 @@ namespace TelegramTestBot.UI
         {
             try
             {
-                string contentQuestion = Tb_ContentQuestuon.Text;
-                string content1Answer = TB_FirstAnswer.Text;
-                string content2Answer = TB_SecondAnswer.Text;
-                string content3Answer = TB_ThirdAnswer.Text;
-                string content4Answer = TB_FourthAnswer.Text;
-                bool firstRB = false;
-                bool secondRB = false;
-                bool thirdRB = false;
-                bool fourthRB = false;
-
-                if (RB_RightAnswer1.IsChecked == true)
+                if (TB_FirstAnswer.Text.Length > 0 && TB_SecondAnswer.Text.Length > 0 && TB_ThirdAnswer.Text.Length > 0 && TB_FourthAnswer.Text.Length > 0 && Tb_ContentQuestuon.Text.Length > 0)
                 {
-                    firstRB = true;
-                    secondRB = false;
-                    thirdRB = false;
-                    fourthRB = false;
-                }
-                else if (RB_RightAnswer2.IsChecked == true)
-                {
-                    firstRB = true;
-                    secondRB = true;
-                    thirdRB = false;
-                    fourthRB = false;
-                }
-                else if(RB_RightAnswer3.IsChecked == true)
-                {
-                    firstRB = false;
-                    secondRB = false;
-                    thirdRB = true;
-                    fourthRB = false;
-                }
-                else if(RB_RightAnswer4.IsChecked == true)
-                {
-                    firstRB = false;
-                    secondRB = false;
-                    thirdRB = false;
-                    fourthRB = true;
-                }
 
-                int testId = _createdTestId;
+                    string contentQuestion = Tb_ContentQuestuon.Text;
+                    string content1Answer = TB_FirstAnswer.Text;
+                    string content2Answer = TB_SecondAnswer.Text;
+                    string content3Answer = TB_ThirdAnswer.Text;
+                    string content4Answer = TB_FourthAnswer.Text;
+                    bool firstRB = false;
+                    bool secondRB = false;
+                    bool thirdRB = false;
+                    bool fourthRB = false;
 
-                testService.CreateQuestion(testId,contentQuestion);
+                    if (RB_RightAnswer1.IsChecked == true)
+                    {
+                        firstRB = true;
+                        secondRB = false;
+                        thirdRB = false;
+                        fourthRB = false;
+                    }
+                    else if (RB_RightAnswer2.IsChecked == true)
+                    {
+                        firstRB = true;
+                        secondRB = true;
+                        thirdRB = false;
+                        fourthRB = false;
+                    }
+                    else if (RB_RightAnswer3.IsChecked == true)
+                    {
+                        firstRB = false;
+                        secondRB = false;
+                        thirdRB = true;
+                        fourthRB = false;
+                    }
+                    else if (RB_RightAnswer4.IsChecked == true)
+                    {
+                        firstRB = false;
+                        secondRB = false;
+                        thirdRB = false;
+                        fourthRB = true;
+                    }
 
-                testService.CreateAnswer(testId, content1Answer, content2Answer, content3Answer, content4Answer,firstRB,secondRB,thirdRB,fourthRB);
+                    int testId = _createdTestId;
 
-                List<QuestionModel> creatingQuest = _questionModelManager.GetQuestionByTestId(testId);
-                LB_CreatedQuestion.ItemsSource = creatingQuest;
-                Tb_ContentQuestuon.Clear();
-                TB_FirstAnswer.Clear();
-                TB_SecondAnswer.Clear();
-                TB_ThirdAnswer.Clear();
-                TB_FourthAnswer.Clear();
+                    testService.CreateQuestion(testId, contentQuestion);
+
+                    testService.CreateAnswer(testId, content1Answer, content2Answer, content3Answer, content4Answer, firstRB, secondRB, thirdRB, fourthRB);
+
+                    List<QuestionModel> creatingQuest = _questionModelManager.GetQuestionByTestId(testId);
+                    LB_CreatedQuestion.ItemsSource = creatingQuest;
+                    Tb_ContentQuestuon.Clear();
+                    TB_FirstAnswer.Clear();
+                    TB_SecondAnswer.Clear();
+                    TB_ThirdAnswer.Clear();
+                    TB_FourthAnswer.Clear();
+                }
+                else MessageBox.Show("Для создания вопроса нужно ввести содержание вопроса и все 4 ответа!");
 
         }
             catch (Exception)
@@ -322,12 +327,16 @@ namespace TelegramTestBot.UI
 
         private void Button_EditTest_Click(object sender, RoutedEventArgs e)
         {
-            TestModel selectedTest = (TestModel)LB_CreatedTest.SelectedItem;
-            TBox_CreateEdittTest.Text = selectedTest.Name;
-            _createdTestId = selectedTest.Id;
-            GridTest.Visibility = Visibility.Visible;
-            List<QuestionModel> creatingQuest = _questionModelManager.GetQuestionByTestId(_createdTestId);
-            LB_CreatedQuestion.ItemsSource = creatingQuest;
+            if (LB_CreatedTest.SelectedItem != null)
+            {
+                TestModel selectedTest = (TestModel)LB_CreatedTest.SelectedItem;
+                TBox_CreateEdittTest.Text = selectedTest.Name;
+                _createdTestId = selectedTest.Id;
+                GridTest.Visibility = Visibility.Visible;
+                List<QuestionModel> creatingQuest = _questionModelManager.GetQuestionByTestId(_createdTestId);
+                LB_CreatedQuestion.ItemsSource = creatingQuest;
+            }
+            else MessageBox.Show("Для изменения теста, выберите или создайте его");
         }
 
         private void LB_CreatedQuestion_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -364,60 +373,64 @@ namespace TelegramTestBot.UI
         {
             try
             {
-                string contentQuestion = Tb_ContentQuestuon.Text;
-                string content1Answer = TB_FirstAnswer.Text;
-                string content2Answer = TB_SecondAnswer.Text;
-                string content3Answer = TB_ThirdAnswer.Text;
-                string content4Answer = TB_FourthAnswer.Text;
-                bool firstRB = false;
-                bool secondRB = false;
-                bool thirdRB = false;
-                bool fourthRB = false;
-                if (RB_RightAnswer1.IsChecked == true)
+                if (TB_FirstAnswer.Text.Length > 0 && TB_SecondAnswer.Text.Length > 0 && TB_ThirdAnswer.Text.Length > 0 && TB_FourthAnswer.Text.Length > 0 && Tb_ContentQuestuon.Text.Length > 0)
                 {
-                    firstRB = true;
-                    secondRB = false;
-                    thirdRB = false;
-                    fourthRB = false;
-                }
-                else if (RB_RightAnswer2.IsChecked == true)
-                {
-                    firstRB = true;
-                    secondRB = true;
-                    thirdRB = false;
-                    fourthRB = false;
-                }
-                else if (RB_RightAnswer3.IsChecked == true)
-                {
-                    firstRB = false;
-                    secondRB = false;
-                    thirdRB = true;
-                    fourthRB = false;
-                }
-                else if (RB_RightAnswer4.IsChecked == true)
-                {
-                    firstRB = false;
-                    secondRB = false;
-                    thirdRB = false;
-                    fourthRB = true;
-                }
-                int testId = _createdTestId;
+                    string contentQuestion = Tb_ContentQuestuon.Text;
+                    string content1Answer = TB_FirstAnswer.Text;
+                    string content2Answer = TB_SecondAnswer.Text;
+                    string content3Answer = TB_ThirdAnswer.Text;
+                    string content4Answer = TB_FourthAnswer.Text;
+                    bool firstRB = false;
+                    bool secondRB = false;
+                    bool thirdRB = false;
+                    bool fourthRB = false;
+                    if (RB_RightAnswer1.IsChecked == true)
+                    {
+                        firstRB = true;
+                        secondRB = false;
+                        thirdRB = false;
+                        fourthRB = false;
+                    }
+                    else if (RB_RightAnswer2.IsChecked == true)
+                    {
+                        firstRB = true;
+                        secondRB = true;
+                        thirdRB = false;
+                        fourthRB = false;
+                    }
+                    else if (RB_RightAnswer3.IsChecked == true)
+                    {
+                        firstRB = false;
+                        secondRB = false;
+                        thirdRB = true;
+                        fourthRB = false;
+                    }
+                    else if (RB_RightAnswer4.IsChecked == true)
+                    {
+                        firstRB = false;
+                        secondRB = false;
+                        thirdRB = false;
+                        fourthRB = true;
+                    }
+                    int testId = _createdTestId;
 
 
-                testService.EditQuestion(_updatedQuestId,testId, contentQuestion);
+                    testService.EditQuestion(_updatedQuestId, testId, contentQuestion);
 
-                testService.EditAnswer(_updatedQuestId, content1Answer, content2Answer, content3Answer, content4Answer, firstRB, secondRB, thirdRB, fourthRB);
+                    testService.EditAnswer(_updatedQuestId, content1Answer, content2Answer, content3Answer, content4Answer, firstRB, secondRB, thirdRB, fourthRB);
 
-                List<QuestionModel> creatingQuest = _questionModelManager.GetQuestionByTestId(testId);
-                LB_CreatedQuestion.ItemsSource = creatingQuest;
-                Tb_ContentQuestuon.Clear();
-                Tb_ContentQuestuon.Clear();
-                TB_FirstAnswer.Clear();
-                TB_SecondAnswer.Clear();
-                TB_ThirdAnswer.Clear();
-                TB_FourthAnswer.Clear();
-                TB_RightAnswerForQuest.Clear();
-                _updatedQuestId = 0;
+                    List<QuestionModel> creatingQuest = _questionModelManager.GetQuestionByTestId(testId);
+                    LB_CreatedQuestion.ItemsSource = creatingQuest;
+                    Tb_ContentQuestuon.Clear();
+                    Tb_ContentQuestuon.Clear();
+                    TB_FirstAnswer.Clear();
+                    TB_SecondAnswer.Clear();
+                    TB_ThirdAnswer.Clear();
+                    TB_FourthAnswer.Clear();
+                    TB_RightAnswerForQuest.Clear();
+                    _updatedQuestId = 0;
+                }
+                else MessageBox.Show("Для изменения вопроса нужно ввести содержание вопроса и все 4 ответа!");
             }
             catch (Exception)
             {
@@ -426,20 +439,85 @@ namespace TelegramTestBot.UI
         }
 
 
-
         private void Button_EditNameOfTest_Click(object sender, RoutedEventArgs e)
         {
-            TestModel selectedTest = (TestModel)LB_CreatedTest.SelectedItem;
-            TB_NewTestorEditName.Text = selectedTest.Name;
-            _updatedTestId = selectedTest.Id;
+            if (TB_NewTestorEditName.Text.Length > 0)
+            {
+                TestModel selectedTest = (TestModel)LB_CreatedTest.SelectedItem;
+                TB_NewTestorEditName.Text = selectedTest.Name;
+                _updatedTestId = selectedTest.Id;
+            }
+            else MessageBox.Show("Для изменения теста, выберите или создайте его");
         }
         private void Button_EditNameOfTestForNew_Click(object sender, RoutedEventArgs e)
         {
-            TestModel updatedtest = new TestModel { Id = _updatedTestId, Name = TB_NewTestorEditName.Text,TeacherId=_authorizedTeacher };
-            _testModelManager.UpdateTestById(updatedtest);
-            TB_NewTestorEditName.Clear();
-            List<TestModel> test = _testModelManager.GetTestByTeacherId(_authorizedTeacher);
-            LB_CreatedTest.ItemsSource = test;
+            if (TB_NewTestorEditName.Text.Length > 0)
+            {
+                TestModel updatedtest = new TestModel { Id = _updatedTestId, Name = TB_NewTestorEditName.Text, TeacherId = _authorizedTeacher };
+                _testModelManager.UpdateTestById(updatedtest);
+                TB_NewTestorEditName.Clear();
+                List<TestModel> test = _testModelManager.GetTestByTeacherId(_authorizedTeacher);
+                LB_CreatedTest.ItemsSource = test;
+            }
+            else MessageBox.Show("Введите новое название теста");
+        }
+
+        private void Button_AddGroup_Click(object sender, RoutedEventArgs e)
+        {
+            if (TB_newGroup.Text.Length > 0)
+            {
+                string nameOfNewGroup = TB_newGroup.Text;
+                _groupModelManager.AddGroup(new GroupModel { Name = nameOfNewGroup });
+                TB_newGroup.Clear();
+                LB_AllGroups.ItemsSource = _groupModelManager.GetAllGroups();
+            }
+        }
+
+        private void LB_AllGroups_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<GroupModel> groups = _groupModelManager.GetAllGroups();
+            LB_AllGroups.ItemsSource = groups;
+        }
+
+        private void Button_EditGroup_Click(object sender, RoutedEventArgs e)
+        {
+            if (LB_AllGroups.SelectedItem != null)
+            {
+            GroupModel selectedGroup  = (GroupModel) LB_AllGroups.SelectedItem;
+            TB_newGroup.Text = selectedGroup.Name;
+            _updatedGroupId = selectedGroup.Id;
+            }
+        }
+
+        private void Button_UpdateGroupName_Click(object sender, RoutedEventArgs e)
+        {
+            if (TB_newGroup.Text.Length > 0)
+            {
+                GroupModel newGroupName = new GroupModel { Id = _updatedGroupId,Name=TB_newGroup.Text};
+                _groupModelManager.UpdateGroupById(newGroupName);
+                TB_newGroup.Clear();
+                LB_AllGroups.ItemsSource = _groupModelManager.GetAllGroups();
+            }
+        }
+
+        private void Button_DeleteGroup_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+            { 
+               if(LB_AllGroups.SelectedItem != null)
+                {
+                  GroupModel selectedGroup = (GroupModel)LB_AllGroups.SelectedItem;
+                  _groupModelManager.DeleteGroupById(selectedGroup.Id);
+                  LB_AllGroups.ItemsSource = _groupModelManager.GetAllGroups();
+                }
+            }
+            catch(InvalidOperationException)
+            {
+                LB_AllGroups.ItemsSource = _groupModelManager.GetAllGroups();
+            }
+
+
+
         }
     }
 }
