1b8f9e4b7d6cd47d88110cf07dfedfba5c609e57
Maxim Kuzmenko <mway2914@yandex.ru>
2023-05-30 23:38:21 +0300

    add count question in test for student attendence

--- TelegramTestBot/TelegramTestBot.DB/dbo/Stored Procedures/Testing_Student_GetByStudentId.sql
+++ TelegramTestBot/TelegramTestBot.DB/dbo/Stored Procedures/Testing_Student_GetByStudentId.sql
@@ -3,11 +3,14 @@
 AS
 BEGIN
 
-	SELECT TS.Id, TS.CountAnswers,TS.IsAttendance,T.[Date] as DateOfTesting,S.Surname as SurnameOfStudent,S.Firstname as FirstnameOfStudent
-	FROM dbo.[Testing_Student] as TS
-    LEFT JOIN dbo.[Testing] as T on (TS.TestingId = T.Id)
-	LEFT JOIN dbo.[Student] as S on (TS.StudentId = S.Id)
+	SELECT TS.Id, TS.CountAnswers, TS.IsAttendance, Tg.[Date] AS DateOfTesting, S.Surname AS SurnameOfStudent, S.Firstname AS FirstnameOfStudent, T.[Name] AS Testname, COUNT(Q.Id) AS CountQuestions
+    FROM dbo.[Testing_Student] AS TS
+    LEFT JOIN dbo.[Testing] AS Tg ON TS.TestingId = Tg.Id
+    LEFT JOIN dbo.[Test] AS T ON Tg.TestId = T.Id
+    LEFT JOIN dbo.[Question] AS Q ON T.Id = Q.TestId
+    LEFT JOIN dbo.[Student] AS S ON TS.StudentId = S.Id
     WHERE TS.StudentId = @StudentId
+    GROUP BY TS.Id, TS.CountAnswers, TS.IsAttendance, Tg.[Date], S.Surname, S.Firstname, T.[Name]
 
 END
 
